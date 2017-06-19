7d29ac5cd7088344d8331e15c4a5f77492d3b0e9
mparikh15 <parikh.mayank@gmail.com>
2017-06-19 13:48:07 -0500

    Optimizing algorithm to average O(n log n) by using random pivot

--- quicksort.py
+++ quicksort.py
@@ -1,11 +1,17 @@
+import random
+
 ToSort = [2, 52, 3, 76, 643, 43, 12, 1]
 
 
 def quicksort(array):
-    if len(array) < 2 :
+    if len(array) < 2:
         return array
+
     else:
-        pivot = array[0]
+        holder = len(array)
+
+        pivot = array[random.randint(0, holder - 1)]
+
         less = [i for i in array[1:] if i <= pivot]
 
         greater = [i for i in array[1:] if i > pivot]
